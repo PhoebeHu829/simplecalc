1302e19eabcaacc71e15dd991e9334a3dc24550c
Xiaodan(Phoebe) Hu <31476199+PhoebeHu829@users.noreply.github.com>
2019-01-15 23:09:54 -0800

    Update main.swift

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -12,8 +12,24 @@ public class Calculator {
     public func calculate(_ args: [String]) -> Int {
         let simpleOperators = ["+", "-", "*", "/", "%"]
         if args.count == 3 && simpleOperators.contains(args[1]) { //simple operations
-            return simpleOp(args)
-        } else { // complex operations
+            let opt = args[1]
+            let a1 = Int(args[0])!
+            let a2 = Int(args[2])!
+            switch opt {
+            case "+":
+                return a1 + a2
+            case "-":
+                return a1 - a2
+            case "*":
+                return a1 * a2
+            case "/":
+                return a1 / a2
+            case "%":
+                return a1 % a2
+            default:
+                return -1
+            }
+        } else { 
             return complexOp(args)
         }
     }
@@ -22,58 +38,36 @@ public class Calculator {
         return calculate( arg.split(separator: " ").map({ substr in String(substr) }) )
     }
 }
-    
-    private func simpleOp(_ args: [String]) -> Int{
-        let optr = args[1]
-        switch optr {
-        case "+":
-            return Int(args[0])! + Int(args[2])!
-        case "-":
-            return Int(args[0])! - Int(args[2])!
-        case "*":
-            return Int(args[0])! * Int(args[2])!
-        case "/":
-            return Int(args[0])! / Int(args[2])!
-        case "%":
-            return Int(args[0])! % Int(args[2])!
-        default:
-            return -1
-        }
-    }
 
-    private func complexOp(_ args: [String]) -> Int{
-        if args.count == 1 { // ["count"], ["avg"], ["fact"] is 0 (no arguments)
-            return 0
-        }
-        let optr = args.last
-        switch optr {
-        case "count"?, "avg"?:
-            let nums:[Int] = args[0..<args.count - 1].map({x in Int(x)!}) // array of all numbers
-            let sum = nums.reduce(0, +)
-            let count = nums.count
-            if optr == "count" { // count
-                return count
-            } else { //avg
-                return sum / count
-            }
-        case "fact"?:
-            let num = Int(args[0])
-            if let num = num { // assertion
-                return getFactorial(num)
-            }
-            return -1
-        default:
-            return -1
-        }
+private func complexOp(_ args: [String]) -> Int{
+    let len = args.count
+    if len == 1 {
+        return 0
     }
- 
-    // returns factorial of a given number
-    private func getFactorial (_ num: Int) -> Int{
+    let opt = args.last
+    switch opt {
+    case "count":
+        return (len - 1)
+        
+    case "avg":
+        var num = 0
+        for i in 0..<(len - 1)  {
+            num = num + Int(args[i])!
+        }
+        return num / (len - 1)
+        
+    case "fact":
+        let num = Int(args[0])!
+        if num == 0 {
+            return 1
+        }
         var result = 1
-        for i in 1..<num + 1 {
+        for i in 1...num {
             result = result * i
         }
         return result
+    default:
+        return -1
     }
 }
 
