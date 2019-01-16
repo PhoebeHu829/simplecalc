573fb2d1ea2b7fd720d480c35f3193bfa0faa4f8
Xiaodan(Phoebe) Hu <31476199+PhoebeHu829@users.noreply.github.com>
2019-01-15 17:15:54 -0800

    Update main.swift

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,12 +10,73 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        let simpleOperators = ["+", "-", "*", "/", "%"]
+        if args.count == 3 && simpleOperators.contains(args[1]) { //simple operations
+            return simpleOp(args)
+        } else { // complex operations
+            return complexOp(args)
+        }
     }
     
     public func calculate(_ arg: String) -> Int {
         return calculate( arg.split(separator: " ").map({ substr in String(substr) }) )
     }
 }
+    
+    private func simpleOp(_ args: [String]) -> Int{
+        let optr = args[1]
+        switch optr {
+        case "+":
+            return Int(args[0])! + Int(args[2])!
+        case "-":
+            return Int(args[0])! - Int(args[2])!
+        case "*":
+            return Int(args[0])! * Int(args[2])!
+        case "/":
+            return Int(args[0])! / Int(args[2])!
+        case "%":
+            return Int(args[0])! % Int(args[2])!
+        default:
+            return -1
+        }
+    }
+
+    private func complexOp(_ args: [String]) -> Int{
+        if args.count == 1 { // ["count"], ["avg"], ["fact"] is 0 (no arguments)
+            return 0
+        }
+        let optr = args.last
+        switch optr {
+        case "count"?, "avg"?:
+            let nums:[Int] = args[0..<args.count - 1].map({x in Int(x)!}) // array of all numbers
+            let sum = nums.reduce(0, +)
+            let count = nums.count
+            if optr == "count" { // count
+                return count
+            } else { //avg
+                return sum / count
+            }
+        case "fact"?:
+            let num = Int(args[0])
+            if let num = num { // assertion
+                return getFactorial(num)
+            }
+            return -1
+        default:
+            return -1
+        }
+    }
+ 
+    // returns factorial of a given number
+    private func getFactorial (_ num: Int) -> Int{
+        var result = 1
+        for i in 1..<num + 1 {
+            result = result * i
+        }
+        return result
+    }
+}
+
 
 print("UW Calculator v1")
 print("Enter an expression separated by returns:")
